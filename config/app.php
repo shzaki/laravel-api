667531303e9e9cfc38e3439f4557e1af4a752b1f
Sherif Zaki <sherif.zaki@airangel.com>
2017-12-16 22:50:35 +0000

    Setting Up Laravel/Passport

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+		// Newly added
+		Laravel\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
